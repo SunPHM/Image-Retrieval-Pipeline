d0ea3e8e69766968e94a6510f25833bb4b397702
Yang Peng <pengyang823@gmail.com>
2014-06-04 13:46:03 -0400

    library support

--- src/ir/feature/SIFTExtraction.java
+++ src/ir/feature/SIFTExtraction.java
@@ -1,17 +1,17 @@
 package ir.feature;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
-import org.apache.lucene.document.Field;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import net.semanticmetadata.lire.imageanalysis.sift.Extractor;
 import net.semanticmetadata.lire.imageanalysis.sift.Feature;
-import net.semanticmetadata.lire.impl.SiftDocumentBuilder;
 
 public class SIFTExtraction {
 
@@ -19,38 +19,27 @@ public class SIFTExtraction {
 	
 	public static void main(String[] args) throws IOException {
 		// test here
-		BufferedImage img = ImageIO.read(new File("test.jpg"));
+		// test HDFS
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path("test.jpg");
+		BufferedImage img = ImageIO.read(fs.open(path));
 		getFeatures(img);
 	}	
 	
 	public static String[] getFeatures(BufferedImage img) throws IOException {
 	
 		List<Feature> features = extractor.computeSiftFeatures(img);
-		//String[] results = new String[features.size()];
+		String[] results = new String[features.size()];
 		for(int i = 0; i < features.size(); i++){
-			//features.get(i).scale; 
-			//transform scale, orientation
-			//String s = "";
-			//s += features.toString();
-			//results[i] = s;
-			System.out.println(features.toString());
+			String s = features.get(i).scale + " " + features.get(i).orientation + " " + features.get(i).location[0] + 
+					" " + features.get(i).location[1] + " " + features.get(i).toString(); 
+			//System.out.println(s);
+			results[i] = s;
 		}
-		//SiftDocumentBuilder builder = new SiftDocumentBuilder();
-			//Field[] fields = builder.createDescriptorFields(img);
-			//String[] features= new String[fields.length];
-			
-			//for(int i =0 ; i < fields.length;i++){ 
-			//		features[i] = fields[i].toString();
-			//		//fields[i].;
-			//		System.out.println(features[i]);
-			//}
-		
-		// what is the difference between feature and field
 		
 		System.out.println("the number is " + features.size());
-		return null;
-			
-			// about the results
-	}
-
+		return results;
+		}
+	
 }
