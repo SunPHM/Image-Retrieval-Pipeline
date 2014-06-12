dd36b3a4d7e7d28112fd6aed30ee0e0dea6ee9cd
xiaofeng <zhou09ustcer@gmail.com>
2014-06-12 15:44:05 -0400

    fix to run on hdfs and local FS

--- src/ir/main/Pipeline.java
+++ src/ir/main/Pipeline.java
@@ -24,21 +24,38 @@ public class Pipeline {
 	}
 	
 	public static void run(String src, String dst, int topK, int botK){
+		long N=1000000000;
+		long startTime = System.nanoTime();
 		
 		//TODO: call the main entry point of the Feature Extraction
 		String features = dst + "/data/features";// the feature folder
 		FeatureExtraction.extractFeatures(src, dst + "/data/fn.txt", dst+"/data/features/", dst + "/temp/fe/");
 		System.out.println("Features folder:"+features);
+		
+		long EndTime1 = System.nanoTime();
+		
 		//TODO: call the main entry point of the vocabulary construction and frequency generation
 		String fs = dst + "/data/fs.seq";
 		//On HDFS features are in data/features instead of test/data/features ???????
-		String[] args = {"data/features/", fs, dst, "" + topK, "" + botK};
+		features="data/features"; //-------comment this when running on localfilesystem!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		String[] args = {features, fs, dst, "" + topK, "" + botK};
 		VWDriver.run(args);
+		
+		long EndTime2 = System.nanoTime();
+		
 		//TODO: call the main entry point of the Indexing and Searching
-		//before run indexing, need to copy the frequency.txt file to local filesystem(index part reads from localfilesystem)---done
+		//before run indexing, need to copy the frequency.txt file to local filesystem(index part reads from localfilesystem)---done 
 		int clusterNum = topK * botK;
 		Search.runIndexing(dst + "/data/frequency.txt", clusterNum, dst + "/cluster/clusters.txt");
 		//TODO: to test or evaluate here
+		
+		long EndTime3 = System.nanoTime();
+		
+		System.out.println("\n\n*******************************************  Running Time********************************************");
+		System.out.println("Total Running Time :"+(EndTime3-startTime)/N 
+				+"\nFeature Extraction: "+(EndTime1-startTime)/N
+				+"\nVVWDriver: "+(EndTime2-EndTime1)/N
+				+"\nIndexing: "+(EndTime3-EndTime2)/N);
 	 
 	}
 
