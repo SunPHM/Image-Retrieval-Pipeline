9e8a8d613a82b485dd824e0cde0c0ed3c0074b1b
Yang Peng <pengyang823@gmail.com>
2014-10-30 15:17:20 -0400

    second major update

--- src/ir/index/GetMAP.java
+++ src/ir/index/GetMAP.java
@@ -37,75 +37,38 @@ public class GetMAP {
 			System.out.println(str);
 		}
 		
-		
 		getmAP(images,gt, num_docs);
 	}
 	
 
-	public static double getmAP(String images,String gt, long total_images) throws IOException{
-			
+	public static double getmAP(String images,String gt, long total_images){
 		// read all the files with "query"
 		String[] files = HadoopUtil.getListOfFiles(gt);
 		// store the image/mAP pairs
 		HashMap<String,Double> images_AP = new HashMap<String,Double>();
-		
+	
 		for(String file : files){			
 			if(file.contains("query")){
-				BufferedReader reader;
-				//System.out.println("Calculating mAP for query file: " + file);
-				FileSystem fs = FileSystem.get(new Configuration());
-				reader = new BufferedReader(new InputStreamReader(fs.open(new Path(file))));
-				String line = reader.readLine();
-				reader.close();
-				String[] array = line.split(" ");
-				String queryImage = array[0].substring("oxc1_".length()) + ".jpg";
-				System.out.println("Calculating AP for query image " + queryImage);
-				double lowX = Double.parseDouble(array[1]);
-				double lowY = Double.parseDouble(array[2]);
-				double highX = Double.parseDouble(array[3]);
-				double highY = Double.parseDouble(array[4]);
-				String[] features = EvaluateOxbuilds.getPartialImageFeatures(images + "/" + queryImage, lowX, lowY, highX, highY);
-				String query = Search.createQuery_topdown(features);
-				// run query
-				String[] files_search_results = null;
 				try {
-					files_search_results = Search.query(query,(int)total_images);
+					// get features and create query
+					String[] features = getFeatures(file, images);
+					String query = Search.createQuery_topdown(features);
+					// run query
+					String[] files_search_results = null;
+					files_search_results = Search.query(query,(int)total_images);	
+					System.out.println("bucket size: " + total_images +"\t actual searched result size: " + files_search_results.length);
+					double AP = getMAP(file, files_search_results);
+					images_AP.put(file, AP);
 				} catch (SolrServerException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				System.out.println("bucket size: " + total_images +"\t actual searched result size: " + files_search_results.length);
-				
-				String query_image_prefix = file.substring(0, file.length() - "_query.txt".length());
-				HashSet<String> goodSet = EvaluateOxbuilds.getFiles( query_image_prefix+ "_good.txt");
-				HashSet<String> okSet = EvaluateOxbuilds.getFiles(query_image_prefix + "_ok.txt");
-				HashSet<String> junkSet = EvaluateOxbuilds.getFiles(query_image_prefix + "_junk.txt");
-				
-				String[] cleaned_results = clean_array(files_search_results,junkSet);
-				
-				System.out.println("cleaned results size: " + cleaned_results.length + "\t junk size " + junkSet.size());
-				
-				double AP = 0;
-				double num_relevant_docs = okSet.size() + goodSet.size();
-				
-				int relevant_detected = 0;
-				for(int i = 0; i<cleaned_results.length; i++){
-					int ok_num = EvaluateOxbuilds.getMatches(cleaned_results, i, okSet);
-					int good_num = EvaluateOxbuilds.getMatches(cleaned_results, i, goodSet);
-					int relevant = getRelevance(cleaned_results[i],okSet,goodSet);
-					if(relevant == 1 ) relevant_detected ++;
-					double precision = ((double) ok_num + good_num) / (i + 1);
-					AP += ( precision * relevant ) / num_relevant_docs;
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				System.out.println(relevant_detected + "\t" + num_relevant_docs);
-				if(relevant_detected != num_relevant_docs){
-					System.out.println("Error found that actual relevant docs and relevant detected  : "
-							 + num_relevant_docs + "  " + relevant_detected + ", please check");
-				}
-					
-				System.out.println("AP for image " + queryImage + " is " + AP);
-				images_AP.put(queryImage, AP);
 			}
+
 		}
 		
 		//TODO get the average mAP of all images' mAP
@@ -121,7 +84,53 @@ public class GetMAP {
 		return avg_mAP;
 	}
 	
-
+	public static double getMAP(String file, String[] files_search_results){
+		// calculate mAP
+		String query_image_prefix = file.substring(0, file.length() - "_query.txt".length());
+		HashSet<String> goodSet = EvaluateOxbuilds.getFiles( query_image_prefix+ "_good.txt");
+		HashSet<String> okSet = EvaluateOxbuilds.getFiles(query_image_prefix + "_ok.txt");
+		HashSet<String> junkSet = EvaluateOxbuilds.getFiles(query_image_prefix + "_junk.txt");
+		
+		String[] cleaned_results = clean_array(files_search_results,junkSet);
+		
+		System.out.println("cleaned results size: " + cleaned_results.length + "\t junk size " + junkSet.size());
+		
+		double AP = 0;
+		double num_relevant_docs = okSet.size() + goodSet.size();
+		
+		int relevant_detected = 0;
+		for(int i = 0; i<cleaned_results.length; i++){
+			int relevant = getRelevance(cleaned_results[i],okSet,goodSet);
+			relevant_detected += relevant;
+			double precision = ((double) relevant_detected) / (i + 1);
+			AP += ( precision * relevant ) / num_relevant_docs;
+		}
+		System.out.println(relevant_detected + "\t" + num_relevant_docs);
+		if(relevant_detected != num_relevant_docs){
+			System.out.println("Error found that actual relevant docs and relevant detected  : "
+					 + num_relevant_docs + "  " + relevant_detected + ", please check");
+		}
+		return AP;
+	}
+	
+	public static String[] getFeatures(String file, String images) throws IOException{
+		BufferedReader reader;
+		//System.out.println("Calculating mAP for query file: " + file);
+		FileSystem fs = FileSystem.get(new Configuration());
+		reader = new BufferedReader(new InputStreamReader(fs.open(new Path(file))));
+		String line = reader.readLine();
+		reader.close();
+		String[] array = line.split(" ");
+		String queryImage = array[0].substring("oxc1_".length()) + ".jpg";
+		System.out.println("Calculating AP for query image " + queryImage);
+		double lowX = Double.parseDouble(array[1]);
+		double lowY = Double.parseDouble(array[2]);
+		double highX = Double.parseDouble(array[3]);
+		double highY = Double.parseDouble(array[4]);
+		String[] features = EvaluateOxbuilds.getPartialImageFeatures(images + "/" + queryImage, lowX, lowY, highX, highY);
+		return features;
+	}
+		
 	private static int getRelevance(String string, HashSet<String> okSet,
 			HashSet<String> goodSet) {
 		// TODO Auto-generated method stub
